4d388dfb79e9774b26c1a65753fd822bcba39554
Eric Gagné <egagne@cstj.qc.ca>
2021-04-18 13:44:59 -0400

    Frontend initial

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ### La branche du cours 9
 
 Cette branche contient le code pour:
-- Le projet SWAPI final dans `/swapi-final`
+- Le dossier `/src` de départ (contient fichiers de base et css): vous pouvez copier ce dossier dans votre projet Vue de base et écraser le dossier `/src` qui s'y trouve déjà.
+    - Le projet de départ **n'est pas fonctionnel**.
+- Le projet SWAPI final dans `/swapi-final` (n'inclut que le dossier `/src`)
 
 L'API utilisé est SWAPI, un API sur [Star Wars accessible ici](https://swapi.dev/).
---
+++ swapi-depart/src/SWAPI.js
@@ -0,0 +1,48 @@
+import { http_get } from './utils/request';
+
+const api_url = 'https://swapi.dev/api';
+const format = 'json';
+
+export default class SWAPI {
+    /**
+     * Retourne tous les films
+     * 
+     * @returns Array
+     */
+    static films() {
+        return this.getAllPagesRecursive(`${api_url}/films/?format=${format}`);
+    }
+
+    /**
+     * Retourne tous les personnages
+     * 
+     * @returns Array
+     */
+    static people() {
+        return this.getAllPagesRecursive(`${api_url}/people/?format=${format}`);
+    }
+
+    /**
+     * Retourne toutes les pages de résultats de la requête
+     * 
+     * La méthode utilise la récursivité
+     * 
+     * @param {string} url 
+     * @param {Array} list 
+     * @returns Array 
+     */
+    static getAllPagesRecursive(url, list = []) {
+        return http_get(url).then(data => {
+            list = list.concat(data.results);
+
+            //console.log(data);
+
+            if (data.next) {
+                return this.getAllPagesRecursive(data.next, list);
+
+            } else {
+                return list;
+            }
+        });
+    }
+}
