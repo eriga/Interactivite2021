953ef0aebeae98c9c6de955a74912bb9904a4ce3
Eric Gagné <egagne@cstj.qc.ca>
2021-05-16 17:15:44 -0400

    Suppression branche 17

--- exercice-sort-demo/js/router.js
+++
@@ -1,9 +0,0 @@
-import List from './pages/List'
-import Cart from './pages/Cart'
-
-export default new VueRouter({
-    routes: [
-        { path: '/', component: List },
-        { path: '/cart', component: Cart },
-    ],
-})
--- panier-paypal-final/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// Ajout d'une feuille de style à Vue
-import './assets/css/style.css'
-
-createApp(App).use(router).mount('#app')
